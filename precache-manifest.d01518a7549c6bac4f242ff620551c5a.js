fc20881f8ea3ed36d3c7850e1026c4ba702ab502
Konstantinos Panteli <konstantinos-panteli@hotmail.com>
2018-08-23 12:03:41 +0300

    c8f8a10

---
+++ precache-manifest.d01518a7549c6bac4f242ff620551c5a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "a79198812a4099b945731965a44192b8",
+    "url": "/img/awm17cm.a7919881.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4a504dbf22c1946d03d23693081b8059",
+    "url": "/img/flipoff.4a504dbf.jpg"
+  },
+  {
+    "revision": "439d9aebc9cba9009434",
+    "url": "/css/app.75d7c982.css"
+  },
+  {
+    "revision": "ced24cec62198fa3627d",
+    "url": "/js/chunk-vendors.753ef63e.js"
+  },
+  {
+    "revision": "f2696f802dfa8118ded7",
+    "url": "/css/store.3da477f6.css"
+  },
+  {
+    "revision": "f2696f802dfa8118ded7",
+    "url": "/js/store.03405509.js"
+  },
+  {
+    "revision": "e43a5fc28a027d1031cab0b6cff4db34",
+    "url": "/img/blades.e43a5fc2.jpg"
+  },
+  {
+    "revision": "bd185b387ae1271c7c566ce8320ba351",
+    "url": "/img/akm21cm.bd185b38.jpg"
+  },
+  {
+    "revision": "4981a2e6d8906d3c1ed5e7c04381024b",
+    "url": "/img/cloud.4981a2e6.jpg"
+  },
+  {
+    "revision": "3279759f0561a096f554f69ca832d5b5",
+    "url": "/img/sawtooth.3279759f.jpg"
+  },
+  {
+    "revision": "20466d475b77aadcea1919646baa9d17",
+    "url": "/img/awm10cm.20466d47.jpg"
+  },
+  {
+    "revision": "59d725ecbe2ba7d549e82b56f64aaea3",
+    "url": "/img/ironam.59d725ec.jpg"
+  },
+  {
+    "revision": "57f67c5b39d00a35d4f47629cd2e305f",
+    "url": "/img/fryingpan.57f67c5b.jpg"
+  },
+  {
+    "revision": "4fdf483b19565ee6e04205814c66adb7",
+    "url": "/img/kar98.4fdf483b.jpg"
+  },
+  {
+    "revision": "3edd9757d11f51644068",
+    "url": "/js/about.7e36b8ac.js"
+  },
+  {
+    "revision": "439d9aebc9cba9009434",
+    "url": "/js/app.9105b2bf.js"
+  },
+  {
+    "revision": "eb114df30104a64920b2ea683460b464",
+    "url": "/img/karshark.eb114df3.jpg"
+  },
+  {
+    "revision": "ea1e3f1ea5e1f10c65f23a85136ba4f5",
+    "url": "/img/ogpick.ea1e3f1e.jpg"
+  },
+  {
+    "revision": "e9f7f1287d93f8a8bb9981761c1cd07e",
+    "url": "/img/akm10cm.e9f7f128.jpg"
+  },
+  {
+    "revision": "de1d92e4ebcfd0f108659d1dce2179b8",
+    "url": "/img/minecraft.de1d92e4.jpg"
+  },
+  {
+    "revision": "1882a4a5313b4cf57d9247a4c435e3a5",
+    "url": "/img/logo.1882a4a5.jpg"
+  },
+  {
+    "revision": "0d3faa5ee277a471e28edbe471f6574e",
+    "url": "/img/m24.0d3faa5e.jpg"
+  },
+  {
+    "revision": "147709ae2c1ac00504e280767148f883",
+    "url": "/img/millenium.147709ae.jpg"
+  },
+  {
+    "revision": "4929a5a5355d504a26998be19cea68d7",
+    "url": "/img/thor.4929a5a5.jpg"
+  },
+  {
+    "revision": "78b7e71c2d098701d7256949c7d27d11",
+    "url": "/img/rickface.78b7e71c.jpg"
+  },
+  {
+    "revision": "f9b3c70b535558b86b6c79388c641d89",
+    "url": "/img/helmet.f9b3c70b.jpg"
+  },
+  {
+    "revision": "c909fcd1bb0e9d854dc1c2b9edfd3079",
+    "url": "/img/rpg.c909fcd1.jpg"
+  },
+  {
+    "revision": "5d6aab8bd4103b414e852685fc034738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3edd9757d11f51644068",
+    "url": "/css/about.c44d6356.css"
+  }
+];
